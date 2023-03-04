2b4e11cea5fb44e1613a0c83743efbecf4f4115a
Bormental61 <tyranosaurus@yandex.ru>
2023-03-04 17:15:04 +0300

    Initial commit

---
+++ src/main/java/org/example/Period.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Period {
+    NOW, FIVE_DAYS, DB
+}
---
+++ src/main/java/org/example/WeatherModel.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.entity.Weather;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface WeatherModel {
+    void getWeather(String selectedCity, Period period) throws IOException;
+
+    public List<Weather> getSavedToDBWeather();
+}
